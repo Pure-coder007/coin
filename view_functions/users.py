ddfdd159d5046aa59ecaa900b5ce42e46a9e81b2
Dike Kingsley <100721103+engrmarkk@users.noreply.github.com>
2024-05-14 18:39:21 +0100

    Update users.py

--- view_functions/users.py
+++ view_functions/users.py
@@ -1,26 +1,24 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
-from decorator import user_required
+from decorator import user_required, check_logged_in
 
 user = Blueprint("users", __name__)
 
 
 @user.route('/')
+@check_logged_in
 def index():
     return render_template('index.html')
 
 
-@user.route('/login', methods=['GET', 'POST'])
-def login():
-    return render_template('login.html')
-
-
 @user.route('/about', methods=['GET', 'POST'])
+@check_logged_in
 def about():
     return render_template('about.html')
 
 
 @user.route('/bitcoin_chart', methods=['GET', 'POST'])
+@check_logged_in
 def bitcoin_chart():
     return render_template('bitcoin_chart.html')
 
@@ -33,41 +31,43 @@ def user_dash():
 
 
 @user.route('/blog', methods=['GET', 'POST'])
+@check_logged_in
 def blog():
     return render_template('blog.html')
 
 
 @user.route('/faq', methods=['GET', 'POST'])
+@check_logged_in
 def faq():
     return render_template('faq.html')
 
 
 @user.route('/investment_plan', methods=['GET', 'POST'])
+@check_logged_in
 def investment_plan():
     return render_template('investment_plan.html')
 
 
 @user.route('/server_error_page', methods=['GET', 'POST'])
+@check_logged_in
 def server_error_page():
     return render_template('server_error_page.html')
 
 
 @user.route('/service', methods=['GET', 'POST'])
+@check_logged_in
 def service():
     return render_template('service.html')
 
 
-@user.route('/sign_up', methods=['GET', 'POST'])
-def sign_up():
-    return render_template('sign_up.html')
-
-
 @user.route('/team', methods=['GET', 'POST'])
+@check_logged_in
 def team():
     return render_template('team.html')
 
 
 @user.route('/terms_of_service', methods=['GET', 'POST'])
+@check_logged_in
 def terms_of_service():
     return render_template('terms_of_service.html')
 
@@ -78,21 +78,28 @@ def error_404():
 
 
 @user.route('/contact', methods=['GET', 'POST'])
+@check_logged_in
 def contact():
     return render_template('contact.html')
 
 
 @user.route('/reset_password', methods=['GET', 'POST'])
+@login_required
+@user_required
 def reset_password():
     return render_template('reset_password.html')
 
 
 @user.route('/add_funds', methods=['GET', 'POST'])
+@login_required
+@user_required
 def add_funds():
     return render_template('add_funds.html')
 
 
 @user.route('/withdrawal', methods=['GET', 'POST'])
+@login_required
+@user_required
 def withdrawal():
     return render_template('withdrawal.html')
 
@@ -107,33 +114,6 @@ def update_mobile():
     return render_template('update_mobile.html')
 
 
-@user.route('/account', methods=['GET', 'POST'])
-def account():
-    return render_template('account.html')
-
-
-
-@user.route('/admin', methods=['GET', 'POST'])
-def admin():
-    return render_template('admin.html')
-
-
-
-@user.route('/bonus_profit', methods=['GET', 'POST'])
-def bonus_profit():
-    return render_template('bonus_profit.html')
-
-
-
-@user.route('/net_profit', methods=['GET', 'POST'])
-def net_profit():
-    return render_template('net_profit.html')
-
-
-
-
-
 @user.route('/layout', methods=['GET', 'POST'])
 def layout():
     return render_template('layout.html')
-
